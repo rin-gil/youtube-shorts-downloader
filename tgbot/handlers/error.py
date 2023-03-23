ea38853b72a0891498a40930bff4b4a37d2b9bc9
rin-gil <91410075+rin-gil@users.noreply.github.com>
2023-03-23 19:03:31 +0200

    fix: Incorrect bot behavior on loading error
    When a raw error occurred while uploading or sending a video, the bot
    did not toggle the state that prohibited the user from sending new
    messages, which eventually made the bot completely inoperable for that
    particular user.
    
    Signed-off-by: rin-gil <91410075+rin-gil@users.noreply.github.com>

--- tgbot/handlers/error.py
+++ tgbot/handlers/error.py
@@ -4,12 +4,22 @@ from aiogram import Dispatcher
 from aiogram.types import Update
 from aiogram.utils.exceptions import TelegramAPIError
 
+from tgbot.middlewares.localization import i18n
 from tgbot.misc.logger import logger
+from tgbot.misc.states import UserInput
+
+_ = i18n.gettext  # Alias for gettext method
 
 
 async def errors_handler(update: Update, exception: TelegramAPIError) -> bool:
     """Logs exceptions that have occurred and are not handled by other functions"""
+    await update.bot.send_message(
+        chat_id=update.message.chat.id,
+        text="❌ " + _("Error downloading the video", locale=update.message.from_user.language_code),
+        reply_to_message_id=update.message.message_id,
+    )
     logger.error("When processing the update with id=%s there was a unhandled error: %s", update.update_id, exception)
+    await UserInput.previous()
     return True
 
 
